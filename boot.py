520c6d6adc0fb015397e3c1ff47345f73818a84f
VitSupawishVuthiwongvarakorn <69845206+vshvit@users.noreply.github.com>
2020-09-11 10:07:59 +0700

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,23 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+# WIFI setup + WebREPL
+import esp
+esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('EGATWIFI', '')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+do_connect()
+import webrepl
+webrepl.start()
+gc.collect()
+
